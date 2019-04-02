76d62ebeb8477d392b7ac3a78f26204d2c7793c6
Brenda Camacho <brendax98@hotmail.com>
2019-04-01 23:47:06 -0600

    final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -39,11 +39,11 @@ var questions = [{
     name: "feature",
     correct: "Her toe beans",
     divClass: ".feature"
-}] // end questions object
+}]
 
 var labels = ["first", "second", "third", "forth"];
 
-// click to start then display quesions
+
 var startGame = $("#start-btn").on('click', function() {
 $(this).parent().hide();
 $('.container').show();
@@ -51,14 +51,14 @@ countdown(30);
 questionDisplay();
 });
 
-// function for displaying questions
+
 var questionDisplay = function() {
 $(".questions :not('#sub-but')").empty();
-// loops through the 10 questions 
+
 for (var j = 0; j < 6; j++) {
 $('.questions').prepend('<div class="' + questions[j].name + '"></div>');
 $(questions[j].divClass).append('<div class ="ques-title">' + questions[j].ques + '</div>');
-// loops through answers for each radio button
+
 for (var i = 0; i <= 3; i++) {
     $(questions[j].divClass).append('<input type="radio"  name="' + questions[j].name + '" value="' + questions[j].ans[i] + '"/><label for="' + labels[i] + '">' + questions[j].ans[i] + '</label>');
 }
@@ -67,7 +67,7 @@ $('.questions').prepend('<hr />');
 }
 
 
-// function for countdown timer
+
 var countdown = function(seconds) {
 
 var timer = setInterval(function() {
@@ -80,7 +80,7 @@ if (seconds <= 0) {
     var wrongAnswers = 0;
     var unAnswered = 0;
 
-    // loop through correctArray & radioName to match html elements & answers
+   
     for (var i = 0; i < 6; i++) {
 
         if ($('input:radio[name="' + questions[i].name + '"]:checked').val() === questions[i].correct) {
@@ -93,31 +93,31 @@ if (seconds <= 0) {
         };
     }
     $('#correctTimesUp').append(correctAnswers);
-    // display wrongAnswers
+    
     $('#wrongTimesUp').append(wrongAnswers);
     $('#timesUp').fadeIn(1000).show();
 
-    // alert("Times Up!");
+   
     clearInterval(timer);
     return;
 }
 }, 1000);
 
-// click event for submit button to stop timer
+
 $('#sub-but').on('click', function() {
 clearInterval(timer);
 })
-}; // end countdown
+}; 
 
 
-// function to grade quiz once submit button is clicked
+
 var gradeQuiz = $('#sub-but').on('click', function() {
 
 var correctAnswers = 0;
 var wrongAnswers = 0;
 var unAnswered = 0;
 
-// loop through correctArray & radioName to match html elements & answers
+
 for (var i = 0; i < 6; i++) {
 
 if ($('input:radio[name="' + questions[i].name + '"]:checked').val() === questions[i].correct) {
@@ -128,17 +128,15 @@ if ($('input:radio[name="' + questions[i].name + '"]:checked').val() === questio
 };
 };
 
-// once submit is clicked...
-// tests
-// stop timer
+
 countdown();
-// fade out questions
+
 $('.container').fadeOut(500);
-// show answerScreen
+
 $('#answerScreen').show();
-// display correctAnswers
+
 $('#correctScreen').append(correctAnswers);
-// display wrongAnswers
+
 $('#wrongScreen').append(wrongAnswers);
 
 }); 
